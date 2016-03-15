95e4f64f0ad3d1897d5c772eb8a02407ec824420
RayChang <phileiny@gmail.com>
2016-03-15 15:57:43 +0800

    environment test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,11 +25,11 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
+  config.assets.debug = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -76,6 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
